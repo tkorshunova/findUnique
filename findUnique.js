5719d010efe3cd43963e4b031ed24a04e67ee4c4
Tetiana Korshunova <tetianakorshunova@Tetianas-MacBook-Pro.local>
2023-07-17 18:00:16 +0300

    Added comments

--- findUnique.js
+++ findUnique.js
@@ -1,3 +1,4 @@
+//lets use memoization
 const cachedResults = new Map();
 
 export default function findUnique (text) {
@@ -11,6 +12,7 @@ export default function findUnique (text) {
     }
 }
 
+//convert string to array of words
 function createArrayFromString (str) {
   const stringWithoutLineBreacks = str.replace(/[\r\n]+/gm, ' ');
   const string = stringWithoutLineBreacks.replace(/[^a-zA-Z0-9 ]/g, '')
@@ -18,30 +20,30 @@ function createArrayFromString (str) {
   return arr;
 }
 
+function findUniqueLetterInString (array) {
+  const letters = array.map(i => {
+      return findFirstUniqueLetterInArray(i.split('')); //first find unique character in every word in the string
+  })
+  
+  return findFirstUniqueLetterInArray(letters); //then find first unique character in the collection from previous step
+}
+
 function findFirstUniqueLetterInArray (arr) {
-  const iterant = new Set();
-  const unique = [];
+  const iterant = new Set(); //collection of repeated values
+  const unique = []; //collection of unique values
   
   unique.push(arr[0]);
   
   for(let i = 1; i < arr.length; i++) {
-    if(!iterant.has(arr[i])) {
-      const idx = unique.findIndex(k => k === arr[i]);
-      if(idx >= 0) {
-        const val = unique.splice(idx, 1);
-        iterant.add(val[0]);
+    if(!iterant.has(arr[i])) { //skip if such item is already in the collection of repeated elements
+      const idx = unique.findIndex(k => k === arr[i]); //check if an item is unique
+      if(idx >= 0) { 
+        const val = unique.splice(idx, 1);  // if item is not unique, remove it from "unique" collection...
+        iterant.add(val[0]); //...and add to collection of repeated elements
       } else {
-        unique.push(arr[i])
+        unique.push(arr[i]) //if item is unique add it to "unique" collection
       }
     }  
   }
-  return unique[0]; 
-}
-
-function findUniqueLetterInString (array) {
-    const letters = array.map(i => {
-        return findFirstUniqueLetterInArray(i.split(''));
-    })
-    
-    return findFirstUniqueLetterInArray(letters);
+  return unique[0];
 }
